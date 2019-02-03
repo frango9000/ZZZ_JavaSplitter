05a7930e6bc91806887329ee69b22b4360d51288
Francisco Javier Sanchez Temprano <fsancheztemprano@danielcastelao.org>
2019-02-03 12:14:51 +0100

    5.38

--- src/DaLi_05.java
+++ src/DaLi_05.java
@@ -11,7 +11,7 @@ import static lib.Misc.Randomizer.*;
 public class DaLi_05 {
     public static void main(String[] args) {
 
-        ex35();
+        ex41();
 
     }
 
@@ -405,7 +405,32 @@ public class DaLi_05 {
         println("In octal " + Integer.toOctalString(scanInt()) );
     }
     public static void ex39() {
+        final double COMMISSION_SOUGHT = 30000;
+        double salesAmount, 				// Sales amount
+                commission,				// Employee commission
+                balance;					// Sales balance
+        salesAmount = 0;					// Initialize accumulator to 0
+        do {
+            balance = commission = 0;	// Set balance and commission to 0
+            salesAmount += 0.01;			// Increase sales amount by $0.01
 
+            // If sales amount is $10,000.01 and above commission rate is 12%
+            if (salesAmount > 10000)
+                commission += (balance = salesAmount - 10000) * 0.12;
+
+            // If sales amount is $5,000.01-$10,000 commission rate is 10%
+            if (salesAmount > 5000)
+                commission += (balance -= balance - 5000) * 0.10;
+
+            // If sales amount is $0.01-$5,000 commission rate is 8%
+            if (salesAmount > 0)
+                commission += balance * 0.08;
+
+            // While commission is less than commission sought loop
+        } while (commission < COMMISSION_SOUGHT);
+
+        // Display results
+        System.out.printf("Minimum sales to earn $30,000: $%.0f\n", salesAmount/10);
     }
     public static void ex40() {
 
@@ -441,24 +466,6 @@ public class DaLi_05 {
 
     }
     public static void ex51() {
-
-    }
-    public static void ex52() {
-
-    }
-    public static void ex53() {
-
-    }
-    public static void ex54() {
-
-    }
-    public static void ex55() {
-
-    }
-    public static void ex56() {
-
-    }
-    public static void ex57() {
 
     }
 }
