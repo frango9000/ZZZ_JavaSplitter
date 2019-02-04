5236482b0c6487aa3e0168b49b31f220fca6fcd1
Francisco Javier Sanchez Temprano <fsancheztemprano@danielcastelao.org>
2019-02-04 19:23:03 +0100

    6.11

--- src/DaLi_06.java
+++ src/DaLi_06.java
@@ -10,7 +10,7 @@ import static lib.Misc.IO.*;
 public class DaLi_06 {
     public static void main(String[] args) {
 
-        ex10(100000);
+        ex12(10,20,4);
 
     }
 
@@ -64,10 +64,35 @@ public class DaLi_06 {
         print(Arrays.toString(Primes.listOfPrimesUntil(number)));
     }
     public static void ex11() {
+        // Display table
+        System.out.println("\n SalesAmount     Commission");
+        System.out.println("-----------------------------");
+        for (double salesAmount = 10000; salesAmount <= 100000; salesAmount += 5000) {
+            System.out.printf(" %-16.0f", salesAmount);
+            System.out.printf("%8.1f\n", ex11computeCommission(salesAmount));
+        }
 
     }
-    public static void ex12() {
+    public static double ex11computeCommission(double salesAmount) {
+        double balance, 					// Holds the balance
+                commission;				// Employee commission
+        balance = commission = 0.0;	// Initialize balance and commission to 0
 
+        // If sales amount is $10.000.01 and above commission rate is 12%
+        if (salesAmount >= 10000.01)
+            commission += (balance = salesAmount - 10000) * 0.12;
+
+        // If sales amount is $5,000.01 to $10,000 commissin rate is 10%
+        if (salesAmount >= 5000.01)
+            commission += (balance -= balance - 5000) * 0.10;
+
+        // If sales amount is $0.01 to $5,000.01 commissin rate is 8%
+        if (salesAmount >= 0.01)
+            commission += balance * 0.08;
+
+        return commission;
+    }
+    public static void ex12(int num1, int num2, int numbersPerLine) {
     }
     public static void ex13() {
 
