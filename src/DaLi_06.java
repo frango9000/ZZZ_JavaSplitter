f55407d1a505573951564436b96b7139a455cd19
Francisco Javier Sanchez Temprano <fsancheztemprano@danielcastelao.org>
2019-02-04 22:42:03 +0100

    6 finish and format

--- src/DaLi_06.java
+++ src/DaLi_06.java
@@ -28,8 +28,8 @@ public class DaLi_06 {
     }
     public static void ex01() {
         for (int i = 0; i < 100; i++) {
-            printf("%7d", Algebra.pentagonalNumber(i) );
-            if  (i%10 == 9 )println("");
+            printf("%7d", Algebra.pentagonalNumber(i));
+            if (i % 10 == 9) println("");
         }
     }
     public static void ex02() {
@@ -43,12 +43,12 @@ public class DaLi_06 {
         println(Extras.reverse(1234));
     }
     public static void ex05() {
-        println((int)Extras.max(5,6,7));
+        println((int) Extras.max(5, 6, 7));
     }
     public static void ex06(int lines) {
         for (int i = 1; i <= lines; i++) {
             for (int j = 1; j <= i; j++) {
-                printf("%3d",j);
+                printf("%3d", j);
             }
             println("");
         }
@@ -58,14 +58,14 @@ public class DaLi_06 {
     }
     public static void ex08() {
         printf("%s %s %s %s %s\n", "Miles", "Kilometers", "|", "Kilometers", "Miles");
-        for (int i = 1, k=20; i <= 10; i++, k+=5) {
-            printf("%-8d %-7.3f %s %-10d %.3f\n",  i, Scales.mileToKilometer(i),"|", k, Scales.kilometerToMile(k));
+        for (int i = 1, k = 20; i <= 10; i++, k += 5) {
+            printf("%-8d %-7.3f %s %-10d %.3f\n", i, Scales.mileToKilometer(i), "|", k, Scales.kilometerToMile(k));
         }
     }
     public static void ex09() {
         printf("%s %s %3s %s %5s\n", "Kilogram", "Pound", "|", "Pound", "Kilogram");
-        for (int k = 1, p=20; k <= 10; k+=2, p+=5) {
-            printf("%-8d %-7.3f %s %-5d %.3f\n",  k, Scales.kilogramToPound(k),"|", p, Scales.poundToKilogram(p));
+        for (int k = 1, p = 20; k <= 10; k += 2, p += 5) {
+            printf("%-8d %-7.3f %s %-5d %.3f\n", k, Scales.kilogramToPound(k), "|", p, Scales.poundToKilogram(p));
         }
 
     }
@@ -83,9 +83,9 @@ public class DaLi_06 {
 
     }
     public static double ex11computeCommission(double salesAmount) {
-        double balance, 					// Holds the balance
-                commission;				// Employee commission
-        balance = commission = 0.0;	// Initialize balance and commission to 0
+        double balance,                    // Holds the balance
+                commission;                // Employee commission
+        balance = commission = 0.0;    // Initialize balance and commission to 0
 
         // If sales amount is $10.000.01 and above commission rate is 12%
         if (salesAmount >= 10000.01)
@@ -102,21 +102,21 @@ public class DaLi_06 {
         return commission;
     }
     public static void ex12(int num1, int num2, int numbersPerLine) {
-        for (int i = num1, j=0; i <= num2; i++) {
+        for (int i = num1, j = 0; i <= num2; i++) {
             printf("%3d", i);
-            if(j++%numbersPerLine==numbersPerLine-1)println("");
+            if (j++ % numbersPerLine == numbersPerLine - 1) println("");
         }
     }
     public static void ex13(int i) {
         float sum = 0;
-        for (int j=1; j<=i ; j++) {
-            sum += j/(j+2f);
+        for (int j = 1; j <= i; j++) {
+            sum += j / (j + 2f);
             printf("%-4d %f %n", j, sum);
         }
     }
     public static void ex14() {
-        for (int i = 1; i < 1000; i+=100) {
-            printf("%-6d %.5f%n",i, ex14pi(i));
+        for (int i = 1; i < 1000; i += 100) {
+            printf("%-6d %.5f%n", i, ex14pi(i));
         }
     }
     public static double ex14pi(int precision) {
@@ -127,9 +127,9 @@ public class DaLi_06 {
     }
     private static void ex15() {
         // Print tax table
-        double from = 50000;		// Start table taxable income range at $50,000
-        double to = 60000;		// End table taxable income range at $60,000
-        double interval = 50;	// Increase each row by $50
+        double from = 50000;        // Start table taxable income range at $50,000
+        double to = 60000;        // End table taxable income range at $60,000
+        double interval = 50;    // Increase each row by $50
 
         // Print header
         System.out.println(
@@ -148,43 +148,46 @@ public class DaLi_06 {
         }
     }
     public static double ex15computeTax(int status, double taxableIncome) {
-        double tax, 				// Total tax
-                taxRate,			// Tax rate
-                incomeTaxed, 		// Taxed income
-                incomeUntaxed, 	// Untaxed income
-                taxAt15Pecent, 	// Minimum marginal amount of 15% tax bracket
-                taxAt25Pecent, 	// Minimum marginal amount of 25% tax bracket
-                taxAt28Pecent, 	// Minimum marginal amount of 28% tax bracket
-                taxAt33Pecent, 	// Minimum marginal amount of 33% tax bracket
-                taxAt35Pecent;	// Minimum marginal amount of 35% tax bracket
+        double tax,                // Total tax
+                taxRate,            // Tax rate
+                incomeTaxed,        // Taxed income
+                incomeUntaxed,    // Untaxed income
+                taxAt15Pecent,    // Minimum marginal amount of 15% tax bracket
+                taxAt25Pecent,    // Minimum marginal amount of 25% tax bracket
+                taxAt28Pecent,    // Minimum marginal amount of 28% tax bracket
+                taxAt33Pecent,    // Minimum marginal amount of 33% tax bracket
+                taxAt35Pecent;    // Minimum marginal amount of 35% tax bracket
 
         // Initialize variables to 0
         taxAt15Pecent = taxAt25Pecent = taxAt28Pecent = taxAt33Pecent =
                 taxAt35Pecent = incomeUntaxed = taxRate = tax = 0.0;
 
         switch (status) { // Get minimum taxableIncome for tax brackets
-            case 0 : // Single
+            case 0: // Single
                 taxAt15Pecent = 8351;
                 taxAt25Pecent = 33951;
                 taxAt28Pecent = 82251;
                 taxAt33Pecent = 171551;
-                taxAt35Pecent = 372951; break;
+                taxAt35Pecent = 372951;
+                break;
 
-            case 1 : // Married Joint or Qualifying Widow(er)
+            case 1: // Married Joint or Qualifying Widow(er)
                 taxAt15Pecent = 16701;
                 taxAt25Pecent = 67901;
                 taxAt28Pecent = 137051;
                 taxAt33Pecent = 208851;
-                taxAt35Pecent = 372951; break;
+                taxAt35Pecent = 372951;
+                break;
 
-            case 2 : // Married Filing Separately
+            case 2: // Married Filing Separately
                 taxAt15Pecent = 8351;
                 taxAt25Pecent = 33951;
                 taxAt28Pecent = 68526;
                 taxAt33Pecent = 104426;
-                taxAt35Pecent = 186476; break;
+                taxAt35Pecent = 186476;
+                break;
 
-            case 3 : // Head of house
+            case 3: // Head of house
                 taxAt15Pecent = 11951;
                 taxAt25Pecent = 45501;
                 taxAt28Pecent = 117451;
@@ -199,27 +202,23 @@ public class DaLi_06 {
             if (taxableIncome >= taxAt35Pecent) {
                 taxRate = 0.35;
                 incomeUntaxed = taxAt35Pecent - 1;
-            }
-            else if (taxableIncome >= taxAt33Pecent) {
+            } else if (taxableIncome >= taxAt33Pecent) {
                 taxRate = 0.33;
                 incomeUntaxed = taxAt33Pecent - 1;
-            }
-            else if (taxableIncome >= taxAt28Pecent) {
+            } else if (taxableIncome >= taxAt28Pecent) {
                 taxRate = 0.28;
                 incomeUntaxed = taxAt28Pecent - 1;
-            }
-            else if (taxableIncome >= taxAt25Pecent) {
+            } else if (taxableIncome >= taxAt25Pecent) {
                 taxRate = 0.25;
-                incomeUntaxed = taxAt25Pecent -1;
-            }
-            else if (taxableIncome >= taxAt15Pecent) {
+                incomeUntaxed = taxAt25Pecent - 1;
+            } else if (taxableIncome >= taxAt15Pecent) {
                 taxRate = 0.15;
                 incomeUntaxed = taxAt15Pecent - 1;
             }
 
             // Accumulate tax for income above the 10% tax bracket
             tax += (incomeTaxed = taxableIncome - incomeUntaxed) * taxRate;
-            taxableIncome -= incomeTaxed;	// Update taxable income
+            taxableIncome -= incomeTaxed;    // Update taxable income
         }
 
         // Accumulate tax for income in the 10% tax bracket
@@ -227,7 +226,7 @@ public class DaLi_06 {
     }
     public static void ex16() {
         int sum = 0;
-        for (int year = 2014; year <= 2034 ; year++) {
+        for (int year = 2014; year <= 2034; year++) {
             sum += Time.daysInYear(year);
         }
         print(sum);
@@ -237,14 +236,14 @@ public class DaLi_06 {
             for (int j = 0; j < n; j++) {
                 printf("%3d", randomInt(2));
             }
-        println("");
+            println("");
         }
     }
     public static void ex18() {
         String password = scanNext("Enter Password\n   at least 10 chars\n   only letters and digits\n   at least 3 digits \n");
-        if( StringManip.countDigits(password) >= 3 && StringManip.countLetters(password) >= 10 && (StringManip.countDigits(password)+StringManip.countLetters(password) == password.length())){
+        if (StringManip.countDigits(password) >= 3 && StringManip.countLetters(password) >= 10 && (StringManip.countDigits(password) + StringManip.countLetters(password) == password.length())) {
             print("Valid");
-        }else print("Invalid");
+        } else print("Invalid");
     }
     public static void ex19() {
         print(Triangle.isValidTriangle(5, 6, 2));
@@ -254,12 +253,12 @@ public class DaLi_06 {
     }
     public static void ex21() {
         String string = scanNext("Enter a string ");
-        StringBuilder number= new StringBuilder();
+        StringBuilder number = new StringBuilder();
 
         for (int i = 0; i < string.length(); i++) {
-            if(DaLi_04.ex15(string.charAt(i)) != -1){
+            if (DaLi_04.ex15(string.charAt(i)) != -1) {
                 number.append(DaLi_04.ex15(string.charAt(i)));
-            }else number.append(string.charAt(i));
+            } else number.append(string.charAt(i));
         }
         print(number);
     }
@@ -272,7 +271,7 @@ public class DaLi_06 {
     public static int ex23(String string, char character) {
         int count = 0;
         for (int i = 0; i < string.length(); i++) {
-            if(string.charAt(i) == character)count++;
+            if (string.charAt(i) == character) count++;
         }
         return count;
     }
@@ -286,62 +285,64 @@ public class DaLi_06 {
         println(Time.converMillis(555550000));
     }
     public static void ex26() {
-        for (long i = 1,j=1; j <= 150; i++) {
-            if(Primes.isPrime(i) && Extras.isPalindrome(i)){
+        for (long i = 1, j = 1; j <= 150; i++) {
+            if (Primes.isPrime(i) && Extras.isPalindrome(i)) {
                 printf("%8d", i);
-                if(j++%10==0)println("");
+                if (j++ % 10 == 0) println("");
             }
         }
     }
     public static void ex27() {
         //List Emirp
-        for (int i = 1,j=1; j <= 120; i++) {
-            if(Extras.isEmirP(i)){
+        for (int i = 1, j = 1; j <= 120; i++) {
+            if (Extras.isEmirP(i)) {
                 printf("%8d", i);
-                if(j++%10==0)println("");
+                if (j++ % 10 == 0) println("");
             }
         }
     }
     public static void ex28() {
         for (int i = 1; i <= 31; i++) {
-            if(Primes.mersennePrime(i) != 0){
-                printf("%8d %8d %n", i,Primes.mersennePrime(i));
+            if (Primes.mersennePrime(i) != 0) {
+                printf("%8d %8d %n", i, Primes.mersennePrime(i));
             }
         }
     }
     public static void ex29() {
         for (int i = 1; i < 1200; i++) {
-            if(Primes.isPrime(i) && Primes.isPrime(i+2)){
-                printf("( %d , %d )%n", i,i+2);
+            if (Primes.isPrime(i) && Primes.isPrime(i + 2)) {
+                printf("( %d , %d )%n", i, i + 2);
             }
         }
     }
     public static boolean ex30() {
-        int roll=ex30rolls();
-        switch(roll){
+        int roll = ex30rolls();
+        switch (roll) {
             case 2:
             case 3:
             case 12:
-                println("You lose");return false;
+                println("You lose");
+                return false;
             case 7:
             case 11:
-                println("You win");return true;
+                println("You win");
+                return true;
             default:
                 printf("Point is %d\n", roll);
-                if(ex30rolls() == roll) {
+                if (ex30rolls() == roll) {
                     println("You win");
                     return true;
-                }else{
+                } else {
                     println("You lose");
                     return false;
                 }
         }
     }
-    public static int ex30rolls(){
-        int dice1 = randomInt(6)+1;
-        int dice2 = randomInt(6)+1;
-        printf("You rolled %d + %d = %d\n", dice1, dice2, dice1+dice2);
-        return dice1+dice2;
+    public static int ex30rolls() {
+        int dice1 = randomInt(6) + 1;
+        int dice2 = randomInt(6) + 1;
+        printf("You rolled %d + %d = %d\n", dice1, dice2, dice1 + dice2);
+        return dice1 + dice2;
 
     }
     public static void ex31() {
@@ -350,9 +351,9 @@ public class DaLi_06 {
         println(number + " is " + (auxp.ch06.CreditCard.isValid(number) ? "valid" : "invalid"));
     }
     public static void ex32() {
-        int wins =0;
+        int wins = 0;
         for (int i = 0; i < 15000; i++) {
-            if(ex30())wins++;
+            if (ex30()) wins++;
         }
         println(wins);
     }
@@ -377,12 +378,12 @@ public class DaLi_06 {
     public static void ex38() {
         for (int i = 1; i <= 200; i++) {
             printf("%c", CharManip.getRandomUpperCaseLetter());
-            if (i%10==0)println("");
+            if (i % 10 == 0) println("");
         }
         println("");
         for (int i = 1; i <= 200; i++) {
             printf("%c", CharManip.getRandomDigitCharacter());
-            if (i%10==0)println("");
+            if (i % 10 == 0) println("");
         }
     }
     public static void ex39() {
