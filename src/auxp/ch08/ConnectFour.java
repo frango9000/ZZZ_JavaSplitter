fc1b83e7cf12d3821d517bcc707836fe000b3ea6
Francisco Javier Sanchez Temprano <fsancheztemprano@danielcastelao.org>
2019-02-20 22:14:18 +0100

    8.20 Connect Four refactor

--- src/auxp/ch08/ConnectFour.java
+++ src/auxp/ch08/ConnectFour.java
@@ -15,7 +15,7 @@ public class ConnectFour {
     public class Board {
         int rows;
         int cols;
-        char[][] board;
+        char[][] table;
 
         public Board() {
             this(6, 7);
@@ -24,20 +24,20 @@ public class ConnectFour {
         public Board(int rows, int cols) {
             this.rows = rows;
             this.cols = cols;
-            board = new char[rows][cols];
+            table = new char[rows][cols];
             newBoard();
         }
 
         void newBoard() {
-            for (int i = 0; i < board.length; i++) {
-                Arrays.fill(board[i], ' ');
+            for (int i = 0; i < table.length; i++) {
+                Arrays.fill(table[i], ' ');
             }
         }
 
         boolean isFull(){
-            for (int i = 0; i < board.length; i++) {
-                for (int j = 0; j < board[i].length; j++) {
-                    if ( board[i][j] == ' ')
+            for (int i = 0; i < table.length; i++) {
+                for (int j = 0; j < table[i].length; j++) {
+                    if ( table[i][j] == ' ')
                         return false;
                 }
             }
@@ -45,17 +45,17 @@ public class ConnectFour {
         }
 
         void printBoard() {
-            for (int i = 0; i < board.length; i++) {
+            for (int i = 0; i < table.length; i++) {
                 String row = "";
-                for (int j = 0, k = 0; k < (board[i].length * 2) + 1; k++) {
+                for (int j = 0, k = 0; k < (table[i].length * 2) + 1; k++) {
                     if (k % 2 == 0)
                         row += "|";
                     else
-                        row += board[i][j++];
+                        row += table[i][j++];
                 }
                 System.out.println(row);
             }
-            char[] end = new char[(board[0].length * 2) + 1];
+            char[] end = new char[(table[0].length * 2) + 1];
             Arrays.fill(end, '-');
             System.out.println(end);
 
@@ -63,39 +63,39 @@ public class ConnectFour {
 
         Character gameOver() {
             //pre-check
-            if (board.length < 4 || board[0].length < 4)
+            if (table.length < 4 || table[0].length < 4)
                 return null;
             if(isFull())
                 return ' ';
             //Rows
-            for (int i = 0; i < board.length; i++) {
-                for (int j = 0; j < board[i].length - 3; j++) {
-                    if (board[i][j] != ' ' & board[i][j] == board[i][j + 1] && board[i][j] == board[i][j + 2] && board[i][j] == board[i][j + 3]) {
-                        return board[i][j];
+            for (int i = 0; i < table.length; i++) {
+                for (int j = 0; j < table[i].length - 3; j++) {
+                    if (table[i][j] != ' ' & table[i][j] == table[i][j + 1] && table[i][j] == table[i][j + 2] && table[i][j] == table[i][j + 3]) {
+                        return table[i][j];
                     }
                 }
             }
             //Cols
-            for (int i = 0; i < board.length - 3; i++) {
-                for (int j = 0; j < board[i].length; j++) {
-                    if (board[i][j] != ' ' & board[i][j] == board[i + 1][j] && board[i][j] == board[i + 2][j] && board[i][j] == board[i + 3][j]) {
-                        return board[i][j];
+            for (int i = 0; i < table.length - 3; i++) {
+                for (int j = 0; j < table[i].length; j++) {
+                    if (table[i][j] != ' ' & table[i][j] == table[i + 1][j] && table[i][j] == table[i + 2][j] && table[i][j] == table[i + 3][j]) {
+                        return table[i][j];
                     }
                 }
             }
             //Diag
-            for (int i = 0; i < board.length - 3; i++) {
-                for (int j = 0; j < board[i].length - 3; j++) {
-                    if (board[i][j] != ' ' & board[i][j] == board[i + 1][j + 1] && board[i][j] == board[i + 2][j + 2] && board[i][j] == board[i + 3][j + 3]) {
-                        return board[i][j];
+            for (int i = 0; i < table.length - 3; i++) {
+                for (int j = 0; j < table[i].length - 3; j++) {
+                    if (table[i][j] != ' ' & table[i][j] == table[i + 1][j + 1] && table[i][j] == table[i + 2][j + 2] && table[i][j] == table[i + 3][j + 3]) {
+                        return table[i][j];
                     }
                 }
             }
             //Diag
-            for (int i = 0; i < board.length - 3; i++) {
-                for (int j = 3; j < board[i].length; j++) {
-                    if (board[i][j] != ' ' & board[i][j] == board[i + 1][j - 1] && board[i][j] == board[i + 2][j - 2] && board[i][j] == board[i + 3][j - 3]) {
-                        return board[i][j];
+            for (int i = 0; i < table.length - 3; i++) {
+                for (int j = 3; j < table[i].length; j++) {
+                    if (table[i][j] != ' ' & table[i][j] == table[i + 1][j - 1] && table[i][j] == table[i + 2][j - 2] && table[i][j] == table[i + 3][j - 3]) {
+                        return table[i][j];
                     }
                 }
             }
@@ -105,23 +105,23 @@ public class ConnectFour {
 
 
         boolean isValidColumn(int column) {
-            if (board[0].length < column || column < 1)
+            if (table[0].length < column || column < 1)
                 return false;
             return lowestRowInColumn(column) >= 0;
         }
 
         void insertIntoColumn(int column, Player player) {
-            board[lowestRowInColumn(column)][column - 1] = player.id;
+            table[lowestRowInColumn(column)][column - 1] = player.id;
         }
 
         int lowestRowInColumn(int column) {
             int columnIndex = column - 1;
-            int lastRow = board.length - 1;
-            if (board[lastRow][columnIndex] == ' ')
+            int lastRow = table.length - 1;
+            if (table[lastRow][columnIndex] == ' ')
                 return lastRow;
             else
-                for (int row = 0; row < board.length; row++) {
-                    if (board[row][columnIndex] != ' ')
+                for (int row = 0; row < table.length; row++) {
+                    if (table[row][columnIndex] != ' ')
                         return row - 1;
 
                 }
