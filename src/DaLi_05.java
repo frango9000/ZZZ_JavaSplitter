f50a569c1aee3a95b125fac255670da8eb28341e
Francisco Javier Sanchez Temprano <fsancheztemprano@danielcastelao.org>
2019-02-28 21:59:47 +0100

    5 tags

--- src/DaLi_05.java
+++ src/DaLi_05.java
@@ -21,7 +21,7 @@ public abstract class DaLi_05 {
 
     }
 
-    public static void ex01() {
+    public static void ex01() {//Pass or fail
         int n = 0;
         while (n != -1) {
             print("Enter Your Grade:");
@@ -30,7 +30,7 @@ public abstract class DaLi_05 {
         }
     }
 
-    public static void ex02() {
+    public static void ex02() {//Repeat multiplications
         int count = 0;
         for (int i = 0; i < 5; i++) {
             int a = randomInt(100), b = randomInt(100), t = 0;
@@ -46,28 +46,28 @@ public abstract class DaLi_05 {
         IO.print("\nCorrect answers = %d", count);
     }
 
-    public static void ex03() {
+    public static void ex03() {//Conversion from Cº to Fº
         IO.print("%s %19s", "Celsius", "Fahrenheit");
         for (int i = 0; i <= 100; i += 2) {
             IO.print("\n%s %25s", i, Scales.celsiusToFahrenheit(i));
         }
     }
 
-    public static void ex04() {
+    public static void ex04() {//Conversion from inch to centimeter
         IO.print("%3s %19s", "Inches", "Centimeter");
         for (int i = 0; i <= 100; i++) {
             IO.print("\n%3d %20.2f", i, Scales.inchToMeter(i) * 100);
         }
     }
 
-    public static void ex05() {
+    public static void ex05() {//Conversion from Cº to Fº and Fº to Cº
         IO.print("%s %14s    |    %10s %11s", "Celsius", "Fahrenheit", "Fahrenheit", "Celsius");
         for (int i = 0; i <= 100; i++) {
             IO.print("\n%3d %18.2f    |    %3d %18.2f", i * 2, Scales.celsiusToFahrenheit(i * 2), i * 5, Scales.fahrenheitToCelsius(i * 5));
         }
     }
 
-    public static void ex06() {
+    public static void ex06() {//Conversion from square meter to ping
         IO.print("%s %17s    |    %12s %9s", "Ping", "Square Meter", "Square Meter", "Ping");
         for (int i = 10; i <= 80; i += 5) {
             IO.print("\n%3d %18.2f    |    %3d %18.2f", i, Scales.pingToSquareMeter(i), i + 20, Scales.fahrenheitToCelsius(i + 20));
@@ -75,7 +75,7 @@ public abstract class DaLi_05 {
 
     }
 
-    public static void ex07() {
+    public static void ex07() {//Financial application: compute future tuition
         float initialV = 10000f;
         float deltaV = 1.06f; // 6%
         float totalCost = 0f;
@@ -89,7 +89,7 @@ public abstract class DaLi_05 {
         print("\nTotal Cost : " + totalCost);
     }
 
-    public static void ex08() {
+    public static void ex08() {//(Find the highest score
         print("Enter num of students: ");
         int numOfStudents = scanInt();
         String bestName = "";
@@ -108,7 +108,7 @@ public abstract class DaLi_05 {
         IO.print("Best student is %s with grade %d", bestName, bestGrade);
     }
 
-    public static void ex09() {
+    public static void ex09() {//Find the two lowest scores)
         print("Enter num of students: ");
         int numOfStudents = scanInt();
         String worstName1 = "", worstName2 = "";
@@ -133,7 +133,7 @@ public abstract class DaLi_05 {
         IO.print("2nd Worst student is %s with grade %d", worstName2, worstGrade2);
     }
 
-    public static void ex10() {
+    public static void ex10() {//Find numbers divisible by 3 and 4
         String log = "";
         int count = 0;
         for (int i = 1; count < 100; i++)
@@ -144,7 +144,7 @@ public abstract class DaLi_05 {
         print(log);
     }
 
-    public static void ex11() {
+    public static void ex11() {//Find numbers divisible by 3 or 4, but not both
         String log = "";
         int count = 0;
         for (int i = 1; count < 100; i++)
@@ -156,21 +156,21 @@ public abstract class DaLi_05 {
 
     }
 
-    public static void ex12() {
+    public static void ex12() {//Find the smallest n such that n3 7 12,000
         int i;
         for (i = 0; !(i * i * i > 1200); i++) {
         }
         IO.print("Answer : %d", i);
     }
 
-    public static void ex13() {
+    public static void ex13() {//Find the largest n such that n2
         int i;
         for (i = 0; i * i < 1200; i++) {
         }
         IO.print("Answer : %d", i);
     }
 
-    public static void ex14() {
+    public static void ex14() {//Compute the greatest common divisor
         print("Enter num 1");
         int n1 = scanInt();
         print("Enter num 2");
@@ -182,7 +182,7 @@ public abstract class DaLi_05 {
         }
     }
 
-    public static void ex15() {
+    public static void ex15() {//Display the ASCII character table
         int lineCount = 0;
         for (int i = '!'; i <= '~'; i++) {
             IO.print("%2c", (char) i);
@@ -190,7 +190,7 @@ public abstract class DaLi_05 {
         }
     }
 
-    public static void ex16() {
+    public static void ex16() {//Find the factors of an integer
         for (int i = 120; i < 130; i++)
             println(i + " : " + Arrays.toString(Primes.getPrimeFactors(i)));
 
@@ -198,7 +198,7 @@ public abstract class DaLi_05 {
             println(i + " : " + Arrays.toString(Primes.getPrimeFactors(i)));
     }
 
-    public static void ex17() {
+    public static void ex17() {//Display pyramid
         int number = 9;
         while (number > 0) {
             for (int i = -number; i <= number; i++) {
@@ -210,7 +210,7 @@ public abstract class DaLi_05 {
         }
     }
 
-    public static void ex18() {
+    public static void ex18() {//Display four patterns using loops
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
                 if (j <= i)
@@ -243,7 +243,7 @@ public abstract class DaLi_05 {
         }
     }
 
-    public static void ex19() {
+    public static void ex19() {//Display numbers in a pyramid pattern
         int lines = 15;
 
         for (int i = 0; i <= lines; i++) {
@@ -257,11 +257,11 @@ public abstract class DaLi_05 {
         }
     }
 
-    public static void ex20() {
+    public static void ex20() {//Display prime numbers between 2 and 1,200
         print(Arrays.toString(Primes.listOfPrimesUntil(1200)));
     }
 
-    public static void ex21() {
+    public static void ex21() {//Financial application: compare loans with various interest rates)
         double loan = scanDouble("Loan Amount: ");
         int years = scanInt("Years: ");
 
@@ -273,7 +273,7 @@ public abstract class DaLi_05 {
         }
     }
 
-    public static void ex22() {
+    public static void ex22() {//Financial application: loan amortization schedule)
         double loanAmount = scanDouble("Loan Amount: ");
         int years = scanInt("Number of Years: ");
         double annualRate = scanDouble("Annual Interest Rate: ");
@@ -295,7 +295,7 @@ public abstract class DaLi_05 {
         }
     }
 
-    public static void ex23() {
+    public static void ex23() {//Demonstrate cancellation errors
         double num = 1;
         for (int i = 1; i < 20; i++) {
             num = num + (num / i);
@@ -309,17 +309,17 @@ public abstract class DaLi_05 {
         println(num);
     }
 
-    public static void ex24() {
+    public static void ex24() {//Sum a series
         println(Algebra.pi());
         println(Math.PI);
     }
 
-    public static void ex25() {
+    public static void ex25() {//Compute p
         println(Algebra.calcE());
         print(Math.E);
     }
 
-    public static void ex26() {
+    public static void ex26() {//Compute e)
         int count = 0;
         for (int i = 2014, l = 1; i <= 2114; i++) {
             if (Time.isLeapYear(i)) {
@@ -334,20 +334,20 @@ public abstract class DaLi_05 {
         println("\nTotal: " + count);
     }
 
-    public static void ex27() {
+    public static void ex27() {//Display leap years
         int year = scanInt("Enter year: ");
         for (int i = 1; i < 13; i++) {
             IO.print("%9s 1, %d is %s\n", Time.monthName(i), year, Time.dayOfWeekString(year, i, 1));
         }
     }
 
-    public static void ex28() {
+    public static void ex28() {//Display the first days of each month
         for (int i = 1; i <= 12; i++) {
             print(Time.consoleCalendar(2019, i));
         }
     }
 
-    public static void ex29() {
+    public static void ex29() {//Display calendars
         float amount = scanFloat("Enter amount ");
         float anualRate = scanFloat("Enter annual interest rate ");
         int months = scanInt("Enter months ");
@@ -358,7 +358,7 @@ public abstract class DaLi_05 {
         }
     }
 
-    public static void ex30() {
+    public static void ex30() {//Financial application: compound value
         float amount = scanFloat("Enter amount ");
         float yield = scanFloat("Enter percentage yield ");
         int months = scanInt("Enter months ");
@@ -368,7 +368,7 @@ public abstract class DaLi_05 {
         }
     }
 
-    public static void ex31() {
+    public static void ex31() {//Financial application: compute CD value
         int number = scanInt("Enter your number ");
         int lotteryA = randomInt(9);
         int lotteryB = lotteryA;
@@ -380,11 +380,11 @@ public abstract class DaLi_05 {
         } else print("You lose. Winner is " + lotteryA + "" + lotteryB);
     }
 
-    public static void ex32() {
+    public static void ex32() {//Game: lottery
         println(Arrays.toString(Algebra.listOfDivisors(28)));
     }
 
-    public static void ex33() {
+    public static void ex33() {//Perfect number
         for (int i = 0; i < 100000; i++) {
             if (Algebra.isPerfectNumber(i)) {
                 println(i);
@@ -392,7 +392,7 @@ public abstract class DaLi_05 {
         }
     }
 
-    public static void ex34() {
+    public static void ex34() {//Game: scissor, rock, paper
         String results = "XXX";
         while (!("AAA".equals(results)) && !("HHH".equals(results))) {
             print("0 rock\n1 paper\n2 scissor\n");
@@ -419,7 +419,7 @@ public abstract class DaLi_05 {
         }
     }
 
-    public static void ex35() {
+    public static void ex35() {//Summation
         double sum = 0;
         for (int i = 1000; i > 0; i--) {
             sum += 1 / (Math.sqrt(i) + Math.sqrt(i - 1));
@@ -432,21 +432,21 @@ public abstract class DaLi_05 {
         println(sum);
     }
 
-    public static void ex36() {
+    public static void ex36() {//Business application: checking ISBN
         DaLi_03.ex09();
     }
 
-    public static void ex37() {
+    public static void ex37() {//Decimal to binary
         int number = scanInt("Enter decimal ");
         println("In binary " + Integer.toBinaryString(scanInt()));
     }
 
-    public static void ex38() {
+    public static void ex38() {//Decimal to octal
         int number = scanInt("Enter decimal ");
         println("In octal " + Integer.toOctalString(scanInt()));
     }
 
-    public static void ex39() {
+    public static void ex39() {//Financial application: find the sales amount
         final double COMMISSION_SOUGHT = 30000;
         double salesAmount,                // Sales amount
                 commission,                // Employee commission
@@ -475,7 +475,7 @@ public abstract class DaLi_05 {
         System.out.printf("Minimum sales to earn $30,000: $%.0f\n", salesAmount / 10);
     }
 
-    public static void ex40() {
+    public static void ex40() {//Simulation: heads or tails
         int heads = 0, tails = 0;
         for (int i = 0; i < 2000000; i++) {
             int coin = randomInt(2);
@@ -485,7 +485,7 @@ public abstract class DaLi_05 {
         IO.print("%d Heads\n%d Tails", heads, tails);
     }
 
-    public static void ex41() {
+    public static void ex41() {//Occurrence of max numbers
         int max = 0, maxCount = 0, newNumber;
         do {
             newNumber = scanInt("Enter a number ");
@@ -498,7 +498,7 @@ public abstract class DaLi_05 {
         IO.print("max number is %d, repeated %d times.", max, maxCount);
     }
 
-    public static void ex42() {
+    public static void ex42() {//Financial application: find the sales amount
         final double COMMISSION_SOUGHT = 30000;
         double salesAmount,                // Sales amount
                 commission = 0,                // Employee commission
@@ -527,7 +527,7 @@ public abstract class DaLi_05 {
         System.out.printf("Minimum sales to earn $30,000: $%.0f\n", salesAmount / 10);
     }
 
-    public static void ex43() {
+    public static void ex43() {//Math: combinations
         int count = 0;
         for (int i = 1; i < 8; i++) {
             for (int j = 1; j < 8; j++) {
@@ -538,7 +538,7 @@ public abstract class DaLi_05 {
         println(count);
     }
 
-    public static void ex44() {
+    public static void ex44() {//Computer architecture: bit-level operations
         byte number = scanByte("Enter a number ");
         String bits = "";
         for (int i = 0; i < 8; i++) {
@@ -548,17 +548,17 @@ public abstract class DaLi_05 {
         IO.print("Bits are %s", bits);
     }
 
-    public static void ex45() {
+    public static void ex45() {//Statistics: compute mean and standard deviation
         double[] numbers = {1, 2, 3, 4.5, 5.6, 6, 7, 8, 9, 10};
         println(Statistics.mean(numbers));
         println(Statistics.standardDeviation(numbers));
     }
 
-    public static void ex46() {
+    public static void ex46() {//Reverse a string
         print(new StringBuilder(scanNextLine("Enter string ")).reverse());
     }
 
-    public static void ex47() {
+    public static void ex47() {//Business: check ISBN-13
         String isbn13string = scanNextLine("Enter the first 12 digits of an ISBN-13 as a string: ");
         int checksum = 0;
         if (isbn13string.length() == 12) {
@@ -570,7 +570,7 @@ public abstract class DaLi_05 {
         } else println("Invalid input");
     }
 
-    public static void ex48() {
+    public static void ex48() {//Process string
         String string = scanNextLine("Enter a string ");
         StringBuilder oddChars = new StringBuilder();
         for (int i = 0; i < string.length(); i += 2) {
@@ -579,7 +579,7 @@ public abstract class DaLi_05 {
         print(oddChars);
     }
 
-    public static void ex49() {
+    public static void ex49() {//Count vowels and consonants
         String string = scanNextLine("Enter a string ");
         int vocal = 0, consonant = 0;
         for (int i = 0; i < string.length(); i++) {
@@ -601,7 +601,7 @@ public abstract class DaLi_05 {
         IO.print("%d vocals %d consonants", vocal, consonant);
     }
 
-    public static void ex50() {
+    public static void ex50() {//Print multiplication table
         int table = 0;
         while (++table < 10) {
             for (int i = 1; i < 10; i++) {
@@ -611,7 +611,7 @@ public abstract class DaLi_05 {
         }
     }
 
-    public static void ex51() {
+    public static void ex51() {//Longest common prefix
         String stringA = scanNextLine("Enter string A ");
         String stringB = scanNextLine("Enter string B ");
         int index = 0;
