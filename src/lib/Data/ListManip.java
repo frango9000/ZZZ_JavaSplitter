9558a6bec9f0e41b61c51ef62e03de76087f24e8
Francisco Javier Sanchez Temprano <fsancheztemprano@danielcastelao.org>
2019-03-06 12:35:17 +0100

    ListManip

--- src/lib/Data/ListManip.java
+++ src/lib/Data/ListManip.java
@@ -5,9 +5,14 @@ import java.util.Arrays;
 
 public class ListManip {
     public static void printList(ArrayList<int[]> list){
-        for ( int[] o:list) {
-            System.out.println(Arrays.toString(o));
+        printList(list, false);
+    }
+    public static void printList(ArrayList<int[]> list, boolean withIndex){
+        String index = "";
+        for (int i = 0; i < list.size(); i++) {
+            if(withIndex)
+                index = i +" ";
+            System.out.println(index + Arrays.toString(list.get(i)));
         }
-
     }
 }
