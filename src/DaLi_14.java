6d5d7e714b8ab8f70c2641ca19dc6a14fe3ea827
Francisco Javier Sanchez Temprano <fsancheztemprano@danielcastelao.org>
2019-03-20 19:48:20 +0100

    14 init

---
+++ src/DaLi_14.java
@@ -0,0 +1,68 @@
+public abstract class DaLi_14 {
+
+    public static void main(String[] args) {
+        ex00();
+
+    }
+
+    public static void ex00() {
+    }
+    public static void ex01() {
+    }
+    public static void ex02() {
+    }
+    public static void ex03() {
+    }
+    public static void ex04() {
+    }
+    public static void ex05() {
+    }
+    public static void ex06() {
+    }
+    public static void ex07() {
+    }
+    public static void ex08() {
+    }
+    public static void ex09() {
+    }
+    public static void ex10() {
+    }
+    public static void ex11() {
+    }
+    public static void ex12() {
+    }
+    public static void ex13() {
+    }
+    public static void ex14() {
+    }
+    public static void ex15() {
+    }
+    public static void ex16() {
+    }
+    public static void ex17() {
+    }
+    public static void ex18() {
+    }
+    public static void ex19() {
+    }
+    public static void ex20() {
+    }
+    public static void ex21() {
+    }
+    public static void ex22() {
+    }
+    public static void ex23() {
+    }
+    public static void ex24() {
+    }
+    public static void ex25() {
+    }
+    public static void ex26() {
+    }
+    public static void ex27() {
+    }
+    public static void ex28() {
+    }
+    public static void ex29() {
+    }
+}
