f9a40978dba8adb0e883e8864169399858047d3c
Francisco Javier Sanchez Temprano <fsancheztemprano@danielcastelao.org>
2019-03-20 20:55:32 +0100

    14.1

--- src/DaLi_14.java
+++ src/DaLi_14.java
@@ -1,13 +1,65 @@
-public abstract class DaLi_14 {
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+
+public class DaLi_14 extends Application {
+
+    public static int WIDTH = 1000;
+    public static int HEIGHT = 600;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        Pane pane = ex01();
+
+
+        Scene scene = new Scene(pane);
+        primaryStage.setTitle("Chapter 14");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 
     public static void main(String[] args) {
-        ex00();
-
+        Application.launch(args);
     }
-
     public static void ex00() {
     }
-    public static void ex01() {
+    public static Pane ex01() {
+        GridPane pane = new GridPane();
+        pane.setAlignment(Pos.CENTER);
+        pane.setPadding(new Insets(10));
+        pane.setHgap(10);
+        pane.setVgap(10);
+
+        String[] images = {"res/image/flag5.gif", "res/image/4.jpg", "res/image/china.gif", "res/image/flag1.gif"};
+        Group g = new Group();
+        for (int i = 0; i < images.length; i++) {
+            Image f = new Image(images[i]);
+            ImageView iv = new ImageView(f);
+
+            iv.setFitHeight(HEIGHT/2);
+            iv.setFitWidth(WIDTH/2);
+
+            switch (i){
+                case 0:
+                    pane.add(iv, 0, 0);break;
+                case 1:
+                    pane.add(iv, 0, 1);break;
+                case 2:
+                    pane.add(iv, 1, 0);break;
+                case 3:
+                    pane.add(iv, 1, 1);break;
+            }
+        }
+        return pane;
     }
     public static void ex02() {
     }
