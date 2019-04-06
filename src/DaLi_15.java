b7a8d6be3a502adc01484debb42ef831e1592897
Francisco Javier Sanchez Temprano <fsancheztemprano@danielcastelao.org>
2019-04-06 11:06:23 +0200

    15.1

--- src/DaLi_15.java
+++ src/DaLi_15.java
@@ -1,13 +1,67 @@
-public class DaLi_15 {
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import lib.Misc.Randomizer;
+
+public class DaLi_15 extends Application{
+
+    private static Stage stage;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        stage = primaryStage;
+        primaryStage.setTitle("C15");
+        //stage.setHeight(500);
+        //stage.setWidth(500);
+
+
+        Pane pane = ex01();
+
+
+        Scene scene = new Scene(pane);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 
     public static void main(String[] args) {
-        ex00();
-
+        Application.launch(args);
     }
 
     public static void ex00() {
     }
-    public static void ex01() {
+    public static Pane ex01() {
+        HBox cards = new HBox();
+        final int elements = 4;
+        for (int i = 0; i < elements; i++) {
+            int rand = Randomizer.randomInt(1, 53);
+            Image img = new Image("res/image/card/" + (rand + 1) + ".png");
+            ImageView iv = new ImageView(img);
+            cards.getChildren().add(iv);
+        }
+
+        Button refreshBtn = new Button("Refresh");
+        refreshBtn.setOnAction(e -> {
+            cards.getChildren().clear();
+            for (int i = 0; i < elements; i++) {
+                int rand = Randomizer.randomInt(1, 53);
+                Image img = new Image("res/image/card/" + (rand + 1) + ".png");
+                ImageView iv = new ImageView(img);
+                cards.getChildren().add(iv);
+            }
+        });
+        BorderPane bp = new BorderPane();
+        bp.setCenter(cards);
+        bp.setBottom(refreshBtn);
+        BorderPane.setAlignment(refreshBtn, Pos.CENTER);
+        return bp;
+
     }
     public static void ex02() {
     }
