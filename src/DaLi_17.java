bbcf3995bc9721bfec98d7a56352f1ff8d5cb7c4
Francisco Javier Sanchez Temprano <fsancheztemprano@danielcastelao.org>
2019-04-22 10:50:58 +0200

    17 init

--- src/DaLi_17.java
+++ src/DaLi_17.java
@@ -26,37 +26,42 @@ public abstract class DaLi_17 {
     }
     public static void ex02() {//Create a binary data file
     }
-
-    public static void ex03() {
+    public static void ex03() {//Sum all the floating points in a binary data file
     }
-
-    public static void ex04() {
+    public static void ex04() {//Convert a text file into UTF
     }
-
-    public static void ex05() {
+    public static void ex05() {//Store objects and arrays in a file
     }
-
-    public static void ex06() {
+    public static void ex06() {//Store Loan objects
     }
-
-    public static void ex07() {
+    public static void ex07() {//Restore objects from a file
     }
-
-    public static void ex08() {
+    public static void ex08() {//Update count
     }
-
-    public static void ex09() {
+    public static void ex09() {//Address book
     }
-
-    public static void ex10() {
+    public static void ex10() {//Split files
     }
-
-    public static void ex11() {
+    public static void ex11() {//Split files GUI
     }
-
-    public static void ex12() {
+    public static void ex12() {//Combine files
     }
-
-    public static void ex13() {
+    public static void ex13() {//Combine files GUI
+    }
+    public static void ex14() {//Encrypt files
+    }
+    public static void ex15() {//Decrypt files
+    }
+    public static void ex16() {//Frequency of characters
+    }
+    public static void ex17() {//BitOutputStream
+    }
+    public static void ex18() {//View bits
+    }
+    public static void ex19() {//View hex
+    }
+    public static void ex20() {//Binary editor
+    }
+    public static void ex21() {//Hex editor
     }
 }
