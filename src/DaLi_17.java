42d1b3f8ae5a43e3a6a5fca90888ff1cd58c5137
Francisco Javier Sanchez Temprano <fsancheztemprano@danielcastelao.org>
2019-04-22 10:47:21 +0200

    17 init

---
+++ src/DaLi_17.java
@@ -0,0 +1,49 @@
+public abstract class DaLi_17 {
+
+    public static void main(String[] args) {
+        ex01();
+
+    }
+
+    public static void ex00() {
+    }
+
+    public static void ex01() {//Create a text file
+
+    }
+    public static void ex02() {//Create a binary data file
+    }
+
+    public static void ex03() {
+    }
+
+    public static void ex04() {
+    }
+
+    public static void ex05() {
+    }
+
+    public static void ex06() {
+    }
+
+    public static void ex07() {
+    }
+
+    public static void ex08() {
+    }
+
+    public static void ex09() {
+    }
+
+    public static void ex10() {
+    }
+
+    public static void ex11() {
+    }
+
+    public static void ex12() {
+    }
+
+    public static void ex13() {
+    }
+}
