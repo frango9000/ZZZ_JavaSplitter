8737c8ecbbcd07e11ada4853e62c12b549de4aca
Francisco Javier Sanchez Temprano <fsancheztemprano@danielcastelao.org>
2019-04-27 13:29:37 +0200

    17.09

--- src/auxp/ch17/AddressBook.java
+++ src/auxp/ch17/AddressBook.java
@@ -37,34 +37,38 @@ public class AddressBook extends BorderPane {
         GridPane grid = new GridPane();
         grid.setVgap(5);
         grid.setHgap(5);
+
+        //set smaller fields size
         stateTF.setPrefColumnCount(2);
         zipTF.setPrefColumnCount(3);
 
+        //set text fields max length
         nameTF.textProperty().addListener(observable -> {
-            if(nameTF.getText().length() > NAME)
-                nameTF.setText(nameTF.getText().substring(0,NAME));
+            if (nameTF.getText().length() > NAME)
+                nameTF.setText(nameTF.getText().substring(0, NAME));
         });
         streetTF.textProperty().addListener(observable -> {
-            if(streetTF.getText().length() > STREET)
-                streetTF.setText(streetTF.getText().substring(0,STREET));
+            if (streetTF.getText().length() > STREET)
+                streetTF.setText(streetTF.getText().substring(0, STREET));
         });
         cityTF.textProperty().addListener(observable -> {
-            if(cityTF.getText().length() > CITY)
-                cityTF.setText(cityTF.getText().substring(0,CITY));
+            if (cityTF.getText().length() > CITY)
+                cityTF.setText(cityTF.getText().substring(0, CITY));
         });
         stateTF.textProperty().addListener(observable -> {
-            if(stateTF.getText().length() > STATE)
-                stateTF.setText(stateTF.getText().substring(0,STATE));
+            if (stateTF.getText().length() > STATE)
+                stateTF.setText(stateTF.getText().substring(0, STATE));
         });
         zipTF.textProperty().addListener(observable -> {
-            if(zipTF.getText().length() > ZIP)
-                zipTF.setText(zipTF.getText().substring(0,ZIP));
+            if (zipTF.getText().length() > ZIP)
+                zipTF.setText(zipTF.getText().substring(0, ZIP));
         });
+
         grid.addRow(0, new Label("Name"), nameTF);
-        grid.addRow(1,new Label("Street"), streetTF);
+        grid.addRow(1, new Label("Street"), streetTF);
         HBox botFields = new HBox(cityTF, new Label("State"), stateTF, new Label("Zip"), zipTF);
         botFields.setSpacing(5);
-        grid.addRow(2,new Label("City"), botFields);
+        grid.addRow(2, new Label("City"), botFields);
 
         Button addBT = new Button("Add");
         Button firstBT = new Button("First");
@@ -80,29 +84,29 @@ public class AddressBook extends BorderPane {
         lastBT.setOnAction(event -> last());
         updateBT.setOnAction(event -> update());
 
-        HBox bot = new HBox(addBT,firstBT,previousBT,nextBT,lastBT,updateBT);
+        HBox bot = new HBox(addBT, firstBT, previousBT, nextBT, lastBT, updateBT);
         bot.setSpacing(10);
         bot.setAlignment(Pos.CENTER);
-        bot.setPadding(new Insets(5,0,0,0));
+        bot.setPadding(new Insets(5, 0, 0, 0));
 
         setCenter(grid);
         setBottom(bot);
 
     }
 
-    void add(){
-        try(RandomAccessFile inout = new RandomAccessFile(file, "rw")){
+    void add() {
+        try (RandomAccessFile inout = new RandomAccessFile(file, "rw")) {
             inout.seek(inout.length());
-            count = (((int)inout.length()) / 91)+1;
+            count = (((int) inout.length()) / 91) + 1;
             write(inout);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    void first(){
-        try(RandomAccessFile inout = new RandomAccessFile(file, "rw")){
-            if(inout.length() > 0){
+    void first() {
+        try (RandomAccessFile inout = new RandomAccessFile(file, "rw")) {
+            if (inout.length() > 0) {
                 inout.seek(0);
                 read(inout);
                 count = 1;
@@ -113,8 +117,8 @@ public class AddressBook extends BorderPane {
         }
     }
 
-    void previous(){
-        try(RandomAccessFile inout = new RandomAccessFile(file, "rw")) {
+    void previous() {
+        try (RandomAccessFile inout = new RandomAccessFile(file, "rw")) {
             if (count > 1)
                 count--;
             else
@@ -127,10 +131,10 @@ public class AddressBook extends BorderPane {
         }
     }
 
-    void next(){
-        try(RandomAccessFile inout = new RandomAccessFile(file, "rw")){
-            if(count * 91 < inout.length()){
-                inout.seek(count*91);
+    void next() {
+        try (RandomAccessFile inout = new RandomAccessFile(file, "rw")) {
+            if (count * 91 < inout.length()) {
+                inout.seek(count * 91);
                 read(inout);
                 count++;
                 System.out.println("Reading address #" + count);
@@ -140,9 +144,9 @@ public class AddressBook extends BorderPane {
         }
     }
 
-    void last(){
-        try(RandomAccessFile inout = new RandomAccessFile(file, "rw")){
-            count = ((int)inout.length()) / 91;
+    void last() {
+        try (RandomAccessFile inout = new RandomAccessFile(file, "rw")) {
+            count = ((int) inout.length()) / 91;
             inout.seek((count * 91) - 91);
             read(inout);
             System.out.println("Reading address #" + count);
@@ -151,8 +155,8 @@ public class AddressBook extends BorderPane {
         }
     }
 
-    void update(){
-        try(RandomAccessFile inout = new RandomAccessFile(file, "rw")){
+    void update() {
+        try (RandomAccessFile inout = new RandomAccessFile(file, "rw")) {
             inout.seek(count * 91 - 91);
             write(inout);
         } catch (IOException e) {
@@ -183,6 +187,7 @@ public class AddressBook extends BorderPane {
         zipTF.setText(new String(zip));
 
     }
+
     void write(RandomAccessFile inout) throws IOException {
         inout.write(fixedLength(nameTF.getText().getBytes(), NAME));
         inout.write(fixedLength(streetTF.getText().getBytes(), STREET));
@@ -194,7 +199,9 @@ public class AddressBook extends BorderPane {
 
     }
 
-    /** Return a byte[] of fixed-length */
+    /**
+     * Return a byte[] of fixed-length
+     */
     private byte[] fixedLength(byte[] x, int n) {
         byte[] b = new byte[n];
         for (int i = 0; i < x.length; i++) {
